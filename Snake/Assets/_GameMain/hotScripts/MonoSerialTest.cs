2d4938781b665ce7ceb594463fa5fc463789901e
nearyc <415004776@qq.com>
2019-03-19 01:03:31 +0800

    1
    1

---
+++ Snake/Assets/_GameMain/hotScripts/MonoSerialTest.cs
@@ -0,0 +1,48 @@
+﻿#if !USE_HOT
+#pragma warning disable 169
+#pragma warning disable 649
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace hot
+{
+    [System.Serializable]
+    class TestListVV
+    {
+        public int value;
+        public string name;
+
+        [System.Serializable]
+        public class TestSSS
+        {
+            public int value;
+            public string name;
+        }
+
+        [SerializeField]
+        TestSSS[] ssss;
+
+        [SerializeField]
+        List<TestSSS> ssss11;
+    }
+
+    [wxb.AutoILMono]
+    class MonoSerialTest
+    {
+        public GameObject go;
+        public int maxValue;
+        public string nameValue;
+        public int[] values;
+
+        public List<int> valuesss;
+        public TestListVV[] ttvaluess;
+        public List<TestListVV> ttvaluesss;
+
+        void Start()
+        {
+            wxb.L.Log("Start");
+        }
+    }
+}
+#endif
